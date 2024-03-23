0660f7e2084c5689b3aa3e72337a1288154c4070
0xWaleed <imwaleed@outlook.sa>
2024-03-23 22:30:21 +0300

    add other platform stub

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <windows.h>
 
 void infinite() {
 	while (1) {
@@ -7,20 +6,43 @@ void infinite() {
 	}
 }
 
+#if defined(__linux__)
+
 int main() {
-	SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
-
-	// in order to get all cpu cores
-	SYSTEM_INFO sys_info;
-	GetSystemInfo(&sys_info);
-
-
-	printf("processors count: %d\n", sys_info.dwNumberOfProcessors);
-
-
-	for (size_t i = 0; i < sys_info.dwNumberOfProcessors - 1; i++) {
-		CreateThread(NULL, 0, infinite, NULL, 0, NULL);
-	}
-
-	infinite(NULL);
+  // do we need it, maybe when I have time?
+  return 0;
 }
+#endif
+
+
+#if defined(__APPLE__)
+int main() {
+  // do we need it, maybe when I have time?
+  return 0;
+}
+#endif
+
+
+#ifdef _WIN32 
+#include <windows.h>
+int main() {
+  SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
+
+  // in order to get all cpu cores
+  SYSTEM_INFO sys_info;
+  GetSystemInfo(&sys_info);
+
+
+  printf("processors count: %d\n", sys_info.dwNumberOfProcessors);
+
+
+  for (size_t i = 0; i < sys_info.dwNumberOfProcessors - 1; i++) {
+    CreateThread(NULL, 0, infinite, NULL, 0, NULL);
+  }
+
+  infinite(NULL);
+
+  return 0;
+}
+#endif
+
